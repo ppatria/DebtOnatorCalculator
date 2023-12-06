d3344f686181e66515649737bd153dd14f7235a1
Prodeo Et Patria <prodeopatria51@gmail.com>
2023-12-05 21:36:59 -0700

    added background color, fixed some conditions in loggin in, changed other aesthetics to the GUI windows,

--- DebtCalculator/src/AutoLoan/AL_Calculations_GUI/AL_Calculations_Controller.java
+++ DebtCalculator/src/AutoLoan/AL_Calculations_GUI/AL_Calculations_Controller.java
@@ -2,6 +2,7 @@ package AutoLoan.AL_Calculations_GUI;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import java.io.File;
@@ -9,17 +10,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javafx.collections.FXCollections;
 
 import AutoLoan.AL_Home_GUI.Main;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class AL_Calculations_Controller {
 
     @FXML
-    private TextField stateTextField;
+    private ComboBox stateComboBox;
     @FXML
     private TextField totalLoanAmountTextField;
     @FXML
@@ -30,6 +35,18 @@ public class AL_Calculations_Controller {
     private TextField loanTermTextField;
     @FXML
     private TextField downPaymentTextField;
+    @FXML
+    private TextField salesTaxTextField;
+    @FXML
+    private TextField additionalFeesTextField;
+    @FXML
+    private TextField minPaymentPerMonthTextField;
+    @FXML
+    private TextField fullyPaidDateTextField;
+    @FXML
+    private TextField totalInterestCostTextField;
+    @FXML
+    private TextField totalCostTextField;
     @FXML
     private Label vehicleInfoLabel;
     @FXML
@@ -43,7 +60,113 @@ public class AL_Calculations_Controller {
     vehicleInfoLabel.setText("Username: " + username + "\n" + 
                              "Vehicle Year: " + vehicleYear + "\n" +
                              "Vehicle Make: " + vehicleMake + "\n" +
-                             "Vehicle Model: " + vehicleModel);
-}
+                             "Vehicle Model: " + vehicleModel + "\n");
+    }
+
+    public void initialize() {
+    stateComboBox.setItems(FXCollections.observableArrayList(
+        "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", 
+        "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA",
+        "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE",
+        "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK",
+        "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT",
+        "VA", "WA", "WV", "WI", "WY"
+        ));
+    }
+
+    public void handleCalculateButtonOnAction(ActionEvent event) {
+
+        Pattern datePattern = Pattern.compile("^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/((19|20)\\d\\d)$");
+        Matcher dateMatcher = datePattern.matcher(originalLoanDateTextField.getText());
+    
+        if(!dateMatcher.matches()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Original loan date shall only accept MM/DD/YYYY dates");
+            alert.showAndWait();
+            return;
+        }
+    
+        Pattern loanTermPattern = Pattern.compile("^\\d{1,3}$");
+        Matcher loanTermMatcher = loanTermPattern.matcher(loanTermTextField.getText());
+    
+        if(!loanTermMatcher.matches()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Loan term shall only accept up to 3 digits");
+            alert.showAndWait();
+            return;
+        }
+        if(stateComboBox.getValue() != null && !totalLoanAmountTextField.getText().isEmpty() &&
+            !originalLoanDateTextField.getText().isEmpty() && !interestRateTextField.getText().isEmpty() && 
+            !loanTermTextField.getText().isEmpty() && !downPaymentTextField.getText().isEmpty() && 
+            !salesTaxTextField.getText().isEmpty() && !additionalFeesTextField.getText().isEmpty()) {
+      
+            double totalLoanAmount = Double.parseDouble(totalLoanAmountTextField.getText());
+            double interestRate = Double.parseDouble(interestRateTextField.getText()) / 100;
+            int loanTerm = Integer.parseInt(loanTermTextField.getText());
+            double downPayment = Double.parseDouble(downPaymentTextField.getText());
+            double salesTax = Double.parseDouble(salesTaxTextField.getText());
+            double additionalFees = Double.parseDouble(additionalFeesTextField.getText());
+            double interestPerMonth = interestRate / 12;
+            
+            double minPaymentPerMonth = (totalLoanAmount+salesTax+additionalFees-downPayment)*(interestPerMonth)/(1-(Math.pow((1+interestPerMonth),-loanTerm)));
+            double totalInterestCost = (minPaymentPerMonth * loanTerm) - (totalLoanAmount + salesTax + additionalFees - downPayment);
+            double totalExpectedCost = totalLoanAmount + salesTax + additionalFees + totalInterestCost - downPayment;
+        
+            minPaymentPerMonthTextField.setText(String.format("%.2f", minPaymentPerMonth));
+            totalInterestCostTextField.setText(String.format("%.2f", totalInterestCost));
+            totalCostTextField.setText(String.format("%.2f", totalExpectedCost));
+        
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            LocalDate originalLoanDate = LocalDate.parse(originalLoanDateTextField.getText(), formatter);
+        
+            LocalDate expectedFullyPaidDate = originalLoanDate.plusMonths(loanTerm);
+            fullyPaidDateTextField.setText(expectedFullyPaidDate.format(formatter));
+      
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill in all the fields.");
+            alert.showAndWait();
+        }
+    }
+
+    public void handleSaveButtonOnAction(ActionEvent event) {
+        try {
+            Path path = Paths.get("output.txt");
+            FileWriter writer = new FileWriter(new File(path.toUri()));
+
+            writer.write("Minimum Payment per Month: " + minPaymentPerMonthTextField.getText() + "\n");
+            writer.write("Total Interest Cost: " + totalInterestCostTextField.getText() + "\n");
+            writer.write("Total Expected Cost: " + totalCostTextField.getText() + "\n");
+            writer.write("Expected Fully Paid Date: " + fullyPaidDateTextField.getText() + "\n");
+            writer.write("Loan term (Months): " + loanTermTextField.getText() + "\n");
+            writer.write("Down payment: " + downPaymentTextField.getText() + "\n");
+
+            writer.close();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Save");
+            alert.setHeaderText(null);
+            alert.setContentText("Information saved.");
+            alert.showAndWait();
+
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("An error occurred while trying to save the information.");
+            alert.showAndWait();
+        }
+    }
+
+
+    public void handleExitButtonOnAction(ActionEvent event) {
+        System.exit(0);
+    }
 
 }
--- DebtCalculator/src/AutoLoan/AL_Home_GUI/AL_Home_Controller.java
+++ DebtCalculator/src/AutoLoan/AL_Home_GUI/AL_Home_Controller.java
@@ -3,6 +3,7 @@ package AutoLoan.AL_Home_GUI;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,56 +12,115 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+
+import AutoLoan.AL_Calculations_GUI.AL_Calculations_Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.PasswordField;
 
 public class AL_Home_Controller {
 
     @FXML
     private TextField usernameTextField;
     @FXML
-    private TextField passwordTextField;
+    private PasswordField passwordTextField;
     @FXML
     private Button homepageSubmitButton;
     @FXML
     private Button newUserButton;
 
     public void homepageSubmit(ActionEvent homepageSubmitEvent) throws IOException{
-        checkUsernameAndPassword();
+        if (usernameTextField.getText().isEmpty() || passwordTextField.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please Enter Username and Password").showAndWait();
+        } else {
+            try{
+                checkUsernameAndPassword();
+            } catch (FileNotFoundException e){
+
+            }
+        }
     }
 
     private void checkUsernameAndPassword() throws FileNotFoundException {
-        String inputUsername = usernameTextField.getText();
-        String inputPassword = passwordTextField.getText();
-        
-        try {
-            File file = new File("resources/" + inputUsername + ".txt");
-            Scanner scanner = new Scanner(file);
-            
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] details = line.split(",");
-
-                if(details[0].equals("username") && details[1].equals(inputUsername)){
-                    if (scanner.hasNextLine()) {
-                        line = scanner.nextLine();
-                        details = line.split(",");
-                        if(details[0].equals("password") && details[1].equals(inputPassword)){
-                            new Alert(Alert.AlertType.INFORMATION, "Login Successful").showAndWait();
-                            return;
-                        }
-                    }
-                    new Alert(Alert.AlertType.ERROR, "Invalid password").showAndWait();
-                    return;
-                }
-            }
-        } catch (FileNotFoundException e) {
+      String inputUsername = usernameTextField.getText();
+      String inputPassword = passwordTextField.getText();
+      String username = null;
+      String vehicleYear = null;
+      String vehicleMake = null;
+      String vehicleModel = null;
+      Scanner scanner = null;
+      
+      try {
+          File file = new File("resources/" + inputUsername + ".txt");
+          scanner = new Scanner(file);
+          while (scanner.hasNextLine()) {
+              String line = scanner.nextLine();
+              String[] details = line.split(",");
+              switch (details[0]) {
+                  case "username":
+                      if (details[1].equals(inputUsername)) {
+                          username = inputUsername;
+                      }
+                      break;
+                  case "password":
+                      if (details[1].equals(inputPassword)) {
+                          // if password correct but username wrong or not present
+                          if (username == null) {
+                              new Alert(Alert.AlertType.ERROR, "Username does not exist").showAndWait();
+                              return;
+                          } else {
+                              new Alert(Alert.AlertType.INFORMATION, "Login Successful").showAndWait();
+                          }
+                      } else {
+                          new Alert(Alert.AlertType.ERROR, "Invalid password").showAndWait();
+                          return;
+                      }
+                      break;
+                  case "year":
+                      vehicleYear = details[1];
+                      break;
+                  case "make":
+                      vehicleMake = details[1];
+                      break;
+                  case "model":
+                      vehicleModel = details[1];
+                      break;
+              }
+          }
+      } catch (FileNotFoundException e) {
+          new Alert(Alert.AlertType.ERROR, "Username does not exist").showAndWait();
+          throw e;
+      } finally {
+          if (scanner != null) {
+            scanner.close();
+      }
+      }
+    
+      // Check all fields are filled correctly
+      if (username != null && vehicleYear != null && vehicleMake != null && vehicleModel != null) {
+          try {
+            existingUser(username, vehicleYear, vehicleMake, vehicleModel);
+          } catch (IOException e) {
             e.printStackTrace();
-        }
-        new Alert(Alert.AlertType.ERROR, "Username does not exist").showAndWait();
+          }
+      } else {
+          new Alert(Alert.AlertType.ERROR, "Missing user or vehicle details").showAndWait();
+      }
+
     }
 
+    // Method to go to Calculations_GUI
+    public void existingUser(String username, String vehicleYear, String vehicleMake, String vehicleModel) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/AutoLoan/AL_Calculations_GUI/AL_Calculations.fxml"));
+        Parent root = loader.load();
+        AL_Calculations_Controller calculationsController = loader.getController();
+        calculationsController.setUserData(username, vehicleYear, vehicleMake, vehicleModel);
+        Main sceneChangerObject = new Main();
+        sceneChangerObject.changeScene(root);
+    }
+
+      // Goes to New_User_GUI
       public void newUser(ActionEvent newUserEvent) throws IOException{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/AutoLoan/AL_New_User_GUI/AL_New_User.fxml"));
         Parent root = loader.load();
